299e9254a3cd11ee316cc9703890b1b6fc23a01b
Steffen Petelka <Steffen.Petelka@hypoport.de>
2014-08-13 13:10:40 +0200

    rechnen

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,59 +1,66 @@
 var honigShopModul = angular.module('honigShopModul', []);
 
 honigShopController = function ($scope, $http) {
-    $scope.bestellung = {
-        einzelPosten: []
-    }
-    $scope.messages = [];
+  $scope.bestellung = {
+    einzelPosten: [],
+    gesamtpreis: 0
+  }
 
-    $http({url: "bestand",
-//    $http({url: "http://localhost:8080/bestand.json",
-        method: "GET",
-        cache: false,
-        headers: {'Content-Type': 'application/json'},
-        timeout: 10000
+  $scope.messages = [];
+
+  $scope.$watch('bestellung', function (newBestellung) {
+    $scope.bestellung.gesamtpreis = 0;
+    $.each(newBestellung.einzelPosten, function (i, posten) {
+      $scope.bestellung.gesamtpreis += posten.getGesamtPreis();
     })
-        .success(function (bestand) {
-            $scope.bestand = bestand;
-            $.each(bestand, function (i, artikel) {
-                $scope.bestellung.einzelPosten.push({artikelId: artikel.id})
-            });
-            $scope.messages.push("Bestandsdaten geladen")
+  }, true)//deep watch
+
+  $http({url: "bestand",
+//    $http({url: "http://localhost:8080/bestand.json",
+          method: "GET",
+          cache: false,
+          headers: {'Content-Type': 'application/json'},
+          timeout: 10000
         })
-        .error(function () {
-            $scope.messages.push("Fehler beim Daten abholen")
-        })
-    $scope.bestellen = function () {
-        $http({url: "bestellung",
+      .success(function (bestand) {
+                 $scope.bestand = bestand;
+                 $.each(bestand, function (i, artikel) {
+                   $scope.bestellung.einzelPosten.push(
+                       {artikelId: artikel.id,
+                         anzahl: 0,
+                         getGesamtPreis: function () {
+                           return this.anzahl * artikel.preis;
+                         }
+                       }
+                   )
+                 });
+                 $scope.messages.push("Bestandsdaten geladen")
+               })
+      .error(function () {
+               $scope.messages.push("Fehler beim Daten abholen")
+             })
+  $scope.bestellen = function () {
+    $http({url: "bestellung",
             method: "POST",
             headers: {'Content-Type': 'application/json'},
             data: JSON.stringify($scope.bestellung)})
 
-            .success(function (bestand) {
-                $scope.messages.push("Erfolgreich gesendet")
-            })
-            .error(function () {
-                $scope.messages.push("Fehler beim Senden")
-            })
-    }
-}
-
-gesamtPreisController = function ($scope) {
-    $scope.gesamtpreis = 0;
-    $scope.change = function () {
-        for (var posten in $scope.bestellung.einzelPosten) {
-            $scope.gesamtpreis += posten.anzahl * posten.preisInCent;
-        }
-    };
+        .success(function (bestand) {
+                   $scope.messages.push("Erfolgreich gesendet")
+                 })
+        .error(function () {
+                 $scope.messages.push("Fehler beim Senden")
+               })
+  }
 }
 
 honigShopModul.filter('preisEuro', function () {
-    return function (input) {
-        input = Number(input);
-        if (isNaN(input)) {
-            throw 'invalid: can\'t convert input';
-        }
-        return (input / 100).toFixed(2) + " €";
-    };
+  return function (input) {
+    input = Number(input);
+    if (isNaN(input)) {
+      throw 'invalid: can\'t convert input';
+    }
+    return (input / 100).toFixed(2) + " €";
+  };
 });
 
