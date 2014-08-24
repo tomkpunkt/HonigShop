35191fe92ee0507b813cf3aa10e4eec24aafdd6f
oliver.otzen@hypoport.de <oliver.otzen@hypoport.de>
2014-08-24 21:39:06 +0200

    BestandController zeigt korrekten Bestand an - begonnen

--- src/main/java/org/hypoport/honig/repository/BestellungRepository.java
+++ src/main/java/org/hypoport/honig/repository/BestellungRepository.java
@@ -1,10 +1,16 @@
 package org.hypoport.honig.repository;
 
 import org.hypoport.honig.model.Bestellung;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BestellungRepository extends MongoRepository<Bestellung, String> {
+public interface BestellungRepository extends CrudRepository<Bestellung, String> {
+
+  @Query("{'einzelPosten.artikelId':?0}")
+  List<Bestellung> findAllWithArtikelId(String id);
 
 }
