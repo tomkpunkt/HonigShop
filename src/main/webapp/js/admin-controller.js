54e73bf56cef3d84b525e9a62346c0d714942f23
oliver.otzen@hypoport.de <oliver.otzen@hypoport.de>
2014-08-28 21:28:48 +0200

    Durch die Sortierung stimmte der Index nicht mehr, wenn man eine Bestellung als bezahlt markiert

--- src/main/webapp/js/admin-controller.js
+++ src/main/webapp/js/admin-controller.js
@@ -1,51 +1,53 @@
 var honigShopAdminModul = angular.module('honigShopAdminModul', []);
 
 var honigShopAdminController = function ($scope, $http) {
-  $scope.bestellungen = []
+    $scope.bestellungen = []
 
-  $scope.messages = [];
+    $scope.messages = [];
 
-  $http({url: "../bestellung",
-          method: "GET"})
-      .success(function (response) {
-                 $scope.messages.push("Erfolgreich empfangen")
-                 $scope.bestellungen = response;
-               })
-      .error(function () {
-               $scope.messages.push("Fehler beim empfangen")
-             });
-
-  $scope.setzeAufBezahlt = function (bestellung, index) {
-    $http({url: "../bestellung/" + bestellung.id + "/bezahlt",
-            method: "PUT"})
-        .success(function (aktualisierteBestellung) {
-                   $scope.messages.push("Erfolgreich gesendet")
-                   $scope.bestellungen[index] = aktualisierteBestellung;
-                 })
+    $http({url: "../bestellung",
+        method: "GET"})
+        .success(function (response) {
+            $scope.messages.push("Erfolgreich empfangen")
+            $scope.bestellungen = response;
+        })
         .error(function () {
-                 $scope.messages.push("Fehler beim senden")
-               });
-  }
+            $scope.messages.push("Fehler beim empfangen")
+        });
+
+    $scope.setzeAufBezahlt = function (bestellung) {
+        $http({url: "../bestellung/" + bestellung.id + "/bezahlt",
+            method: "PUT"})
+            .success(function (aktualisierteBestellung) {
+                $scope.messages.push("Erfolgreich gesendet")
+                var index = $scope.bestellungen.indexOf(bestellung);
+                $scope.bestellungen[index] = aktualisierteBestellung;
+
+            })
+            .error(function () {
+                $scope.messages.push("Fehler beim senden")
+            });
+    }
 }
 
 honigShopAdminModul.filter('preisEuro', function () {
-  return function (input) {
-    input = Number(input);
-    if (isNaN(input)) {
-      throw 'invalid: can\'t convert input';
-    }
-    return (input / 100).toFixed(2) + " €";
-  };
+    return function (input) {
+        input = Number(input);
+        if (isNaN(input)) {
+            throw 'invalid: can\'t convert input';
+        }
+        return (input / 100).toFixed(2) + " €";
+    };
 });
 
 honigShopAdminModul.filter('postenFilter', function () {
-  return function (postenArrays) {
-    var result = "";
-    $.each(postenArrays, function (index, posten) {
-      result = result + posten.anzahl + "x " + posten.artikelId + "\n";
-    })
-    return result;
-  };
+    return function (postenArrays) {
+        var result = "";
+        $.each(postenArrays, function (index, posten) {
+            result = result + posten.anzahl + "x " + posten.artikelId + "\n";
+        })
+        return result;
+    };
 });
 
 
